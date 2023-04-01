3f0852a44096021d8db9fa81a033a044c2694eda
Ariel Jr Mimura <avmimurajr@gmail.com>
2023-04-01 20:20:39 +0800

    Added PayComputeService and aspnetcore mvc nuget package

---
+++ Enterprise.Services/IPayComputationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Enterprise.Entity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Enterprise.Services
+{
+	public interface IPayComputationService
+	{
+		Task CreateAsync(PaymentRecord paymentRecord);
+
+		PaymentRecord GetById(int id);
+
+		IEnumerable<PaymentRecord> GetAll();
+
+		IEnumerable<SelectListItem> GetAlltaxYear();
+
+		decimal OvertimeHours(decimal hoursWorked, decimal contractualHours);
+
+		decimal ContractualEarnings(decimal contractualHours, decimal hoursWorked, decimal hourlyRate);
+
+		decimal OvertimeRate(decimal hourlyRate);
+
+		decimal OvertimeEarnings(decimal overtimeRate, decimal overtimeHours);
+
+		decimal TotalEarnings(decimal overtimeEarnings, decimal contractualEarnings);
+
+		decimal TotalDeductions(decimal tax, decimal sss, decimal studentLoanPayment, decimal unionPayment);
+
+		decimal NetPay(decimal totalEarnings, decimal totalDeduction);
+	}
+}
+
---
+++ Enterprise.Services/Implementations/PayComputationService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Enterprise.Entity;
+using Enterprise.Persistence;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Enterprise.Services.Implementations
+{
+    public class PayComputationService : IPayComputationService
+    {
+        private readonly ApplicationDbContext _context;
+        private decimal contractualEarnings;
+        private decimal overtimeHours;
+
+        public PayComputationService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public decimal ContractualEarnings(decimal contractualHours, decimal hoursWorked, decimal hourlyRate)
+        {
+            if (contractualHours > hoursWorked)
+            {
+                contractualEarnings = hoursWorked * hourlyRate;
+            }
+            else
+            {
+                contractualEarnings = contractualHours * hourlyRate;
+            }
+            return contractualEarnings;
+        }
+
+        public async Task CreateAsync(PaymentRecord paymentRecord)
+        {
+            await _context.PaymentRecords.AddAsync(paymentRecord);
+            await _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<PaymentRecord> GetAll() => _context.PaymentRecords.OrderBy(p => p.EmployeeId);
+
+        public IEnumerable<SelectListItem> GetAlltaxYear()
+        {
+            var allTaxYear = _context.TaxYear.Select(taxYear => new SelectListItem
+            {
+                Text = taxYear.YearOfTax,
+                Value = taxYear.TaxYearId.ToString()
+            });
+            return allTaxYear;
+        }
+
+        public PaymentRecord GetById(int id) =>
+            _context.PaymentRecords.Where(p => p.Id == id).FirstOrDefault();
+
+
+        public decimal NetPay(decimal totalEarnings, decimal totalDeduction) =>
+            totalEarnings - totalDeduction;
+
+        public decimal OvertimeEarnings(decimal overtimeRate, decimal overtimeHours) =>
+            overtimeHours * overtimeRate;
+
+        public decimal OvertimeHours(decimal hoursWorked, decimal contractualHours)
+        {
+            if(hoursWorked<=contractualHours)
+            {
+                overtimeHours = 0.00m;
+            }
+            else if(hoursWorked>contractualHours)
+            {
+                overtimeHours = hoursWorked - contractualHours;
+            }
+            return overtimeHours;
+        }
+
+        public decimal OvertimeRate(decimal hourlyRate) =>
+            hourlyRate * 1.1m;
+
+
+        public decimal TotalDeductions(decimal tax, decimal sss, decimal studentLoanPayment, decimal unionPayment) =>
+            tax + sss + studentLoanPayment + unionPayment;
+
+        public decimal TotalEarnings(decimal overtimeEarnings, decimal contractualEarnings) =>
+            overtimeEarnings + contractualEarnings;
+    }
+}
+
