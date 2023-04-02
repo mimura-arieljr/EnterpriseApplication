0d0a11f326e6c57bae9518eab3b1a1378b0348cc
Ariel Jr Mimura <avmimurajr@gmail.com>
2023-04-02 15:57:36 +0800

    Added Create method in PayController

--- EnterpriseSolution/Controllers/PayController.cs
+++ EnterpriseSolution/Controllers/PayController.cs
@@ -7,18 +7,22 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EnterpriseSolution.Controllers
 {
-	public class PayController : Controller
-	{
-		private readonly IPayComputationService _payComputationService;
+    public class PayController : Controller
+    {
+        private readonly IPayComputationService _payComputationService;
         private readonly IEmployeeService _employeeService;
+        private readonly ISSSComputationService _sssComputationService;
+        private readonly ITaxComputationService _taxComputationService;
 
-		public PayController(IPayComputationService payComputationService, IEmployeeService employeeService)
-		{
-			_payComputationService = payComputationService;
+        public PayController(IPayComputationService payComputationService, IEmployeeService employeeService, ISSSComputationService sssComputationService, ITaxComputationService taxComputationService)
+        {
+            _payComputationService = payComputationService;
             _employeeService = employeeService;
+            _sssComputationService = sssComputationService;
+            _taxComputationService = taxComputationService;
         }
         public IActionResult Index()
-		{
+        {
             {
                 var employee = _payComputationService.GetAll().Select(employee => new PaymentRecordIndexViewModel
                 {
@@ -49,9 +53,20 @@ namespace EnterpriseSolution.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(PaymentRecordCreateViewModel model)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(PaymentRecordCreateViewModel model)
         {
-            if(ModelState.IsValid)
+            decimal overtimeHrs;
+            decimal contractualEarnings;
+            decimal overtimeEarnings;
+            decimal totalEarnings;
+            decimal taxContribution;
+            decimal tax;
+            decimal philHealthPagIbig;
+            decimal loanContribution;
+            decimal totalDeduction;
+
+            if (ModelState.IsValid)
             {
                 var payRecord = new PaymentRecord()
                 {
@@ -64,12 +79,25 @@ namespace EnterpriseSolution.Controllers
                     TaxYearId = model.TaxYearId,
                     HourlyRate = model.HourlyRate,
                     HoursWorked = model.HoursWorked,
-                    ContractualHours = model.ContractualHours
-                    
+                    ContractualHours = model.ContractualHours,
+                    OvertimeHours = overtimeHrs = _payComputationService.OvertimeHours(model.HoursWorked, model.ContractualHours),
+                    ContractualEarnings = contractualEarnings = _payComputationService.ContractualEarnings(model.ContractualHours, model.HoursWorked, model.HourlyRate),
+                    OvertimeEarnings = overtimeEarnings = _payComputationService.OvertimeEarnings(_payComputationService.OvertimeRate(model.HourlyRate), overtimeHrs),
+                    TotalEarnings = totalEarnings = _payComputationService.TotalEarnings(overtimeEarnings, contractualEarnings),
+                    Tax = tax = _taxComputationService.GetTaxDeduction(totalEarnings),
+                    TaxContribution = taxContribution = _sssComputationService.SSSContribution(totalEarnings),
+                    UnionFee = philHealthPagIbig = _employeeService.UnionFees(model.EmployeeId),
+                    LoanContribution = loanContribution = _employeeService.LoanRepaymentAmount(model.EmployeeId, totalEarnings),
+                    TotalDeductions = totalDeduction = _payComputationService.TotalDeductions(tax, taxContribution, philHealthPagIbig, loanContribution),
+                    NetPayment = _payComputationService.NetPay(totalEarnings, totalDeduction)
                 };
+                await _payComputationService.CreateAsync(payRecord);
+                return RedirectToAction(nameof(Index));
             }
+            ViewBag.employees = _employeeService.GetAllEmployeesForPayroll();
+            ViewBag.TaxYears = _payComputationService.GetAlltaxYear();
             return View();
         }
-	}
+    }
 }
 
